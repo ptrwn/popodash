8874830baa583c117c8312f564249d21ec642d46
ptrwn <1374688@gmail.com>
2023-01-26 00:03:51 +0300

    Added data filtering, chart and dropdown.

--- hellodash.py
+++ hellodash.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -8,33 +8,38 @@ from sql_statements import SQL_stmt
 conn = sqlite3.connect('popo.db') 
           
 sql_query = pd.read_sql_query (SQL_stmt.select_full_data, conn)
-
-# df = pd.DataFrame(sql_query, columns = ['product_id', 'product_name', 'price'])
 df = pd.DataFrame(sql_query)
 df.set_axis(['user_id', 'user_name', 'item_id', 'item_name', 'party_id', 'party_name'], axis=1, inplace=True)
 
+parties = df.party_name.unique().tolist()
+
 app = Dash(__name__)
 
-# df = pd.DataFrame({
-#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-#     "Amount": [4, 1, 2, 2, 4, 5],
-#     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-# })
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+app.layout = html.Div([
+    dcc.Dropdown(
+        df.party_name.unique().tolist(),
+        'select the party',
+        id='party-dropdown'
+    ),
+    dcc.Graph(id='users-at-party'),
+    
 ])
+
+
+@app.callback(
+    Output('users-at-party', 'figure'),
+    Input('party-dropdown', 'value'))
+def update_figure(selected_party):
+    df_party = df.loc[df.party_name == selected_party]
+    df_chart = df_party[['user_name', 'item_name']]
+    df_chart = df_chart.groupby(['user_name', 'item_name']).size().reset_index().rename(columns={0: 'count'})
+
+    fig = px.bar(df_chart, x="user_name", y="count", color="item_name", barmode="group")
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
