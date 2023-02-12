d47db0d9c8522564b4b9e92b92e074704209b135
ptrwn <1374688@gmail.com>
2023-02-12 22:34:35 +0300

    Started adaptation to the new DB schema
    - upd drinks csv;
    - upd sql statements;
    - now users, items, locations, and partied are created from dfs.

--- db.py
+++ db.py
@@ -10,6 +10,8 @@ from sql_statements import SQL_stmt
 from random import randint, choice
 from constants import users, locations_to_kinds, NUMBER_OF_GUESTS
 
+from functools import partial
+
 
 
 def create_connection():
@@ -37,51 +39,55 @@ def insert(conn, statement, params):
     return cur.lastrowid
 
 
-loc = list(locations_to_kinds.keys())
+def df_to_sql(conn, df, table_name):
+    df.to_sql(table_name, con=conn, if_exists='replace', index_label='id')
+
 
 def main():
 
-    df_p = pd.read_csv('parties_dates.csv', parse_dates=['start', 'end'])
-    places = list(locations_to_kinds.keys())
-    picked_places = np.random.choice(places, size=df_p.shape[0], replace=True)
-    df_p['location'] = picked_places
-    df_p['id'] = df_p.index
-    df_p.id += 1
-
-    df_i = pd.read_csv('drinks.csv')
+    df_p = pd.read_csv('parties_dates.csv', parse_dates=['start_dt', 'end_dt'])
+    df_u = pd.DataFrame({'name': users})
+    df_i = pd.read_csv('drinks.csv', usecols=['drink', 'abv', 'kind'])
+    df_l = pd.DataFrame({'name': list(locations_to_kinds.keys())})
 
     conn = create_connection()
 
     with conn:
-        df_p.to_sql('party', con=conn, if_exists='replace')
-        create_table(conn, SQL_stmt.create_table_user)
-        create_table(conn, SQL_stmt.create_table_item)
-        create_table(conn, SQL_stmt.create_table_users_parties)
-        create_table(conn, SQL_stmt.create_table_users_items)
 
-        parties = df_p.name.tolist()
+        withconn = partial(df_to_sql, conn)
+        for df, table_name in zip((df_p, df_u, df_i, df_l), ('party', 'user', 'item', 'location')):
+            withconn(df, table_name)
 
-        for party_id, party in enumerate(parties, start=1):
 
-            num_guests = randint(1, NUMBER_OF_GUESTS)
-            party_location =  df_p.loc[df_p.name == party]['location'].item()
-            locations_drinks_kinds = list(locations_to_kinds[party_location])
-            df_loc_items = df_i.loc[df_i['kind'].isin(locations_drinks_kinds)]
+        # df_p.to_sql('party', con=conn, if_exists='replace')
+        # create_table(conn, SQL_stmt.create_table_user)
+        # create_table(conn, SQL_stmt.create_table_item)
+        # create_table(conn, SQL_stmt.create_table_users_parties)
+        # create_table(conn, SQL_stmt.create_table_users_items)
 
-            #TODO: Add processing for exclusive_in
-            df_loc_items = df_loc_items.loc[:, df_loc_items.columns != "exclusive_in"]
+        # parties = df_p.name.tolist()
 
-            for _ in range(num_guests):
-                # TODO add user age
-                user_id = insert(conn, SQL_stmt.insert_user, (choice(users), ))
-                u_p_id = insert(conn, SQL_stmt.insert_user_party, (user_id, party_id))
-                num_items = randint(1, max(2, df_loc_items.shape[0]))
+        # for party_id, party in enumerate(parties, start=1):
 
-                for _ in range(num_items):
-                    item = df_loc_items.sample(1).to_dict('tight')['data'][0]
-                    item = tuple(item)
-                    item_id = insert(conn, SQL_stmt.insert_item, item)
-                    u_i_id = insert(conn, SQL_stmt.insert_user_item, (user_id, item_id))
+        #     num_guests = randint(1, NUMBER_OF_GUESTS)
+        #     party_location =  df_p.loc[df_p.name == party]['location'].item()
+        #     locations_drinks_kinds = list(locations_to_kinds[party_location])
+        #     df_loc_items = df_i.loc[df_i['kind'].isin(locations_drinks_kinds)]
+
+        #     #TODO: Add processing for exclusive_in
+        #     df_loc_items = df_loc_items.loc[:, df_loc_items.columns != "exclusive_in"]
+
+        #     for _ in range(num_guests):
+        #         # TODO add user age
+        #         user_id = insert(conn, SQL_stmt.insert_user, (choice(users), ))
+        #         u_p_id = insert(conn, SQL_stmt.insert_user_party, (user_id, party_id))
+        #         num_items = randint(1, max(2, df_loc_items.shape[0]))
+
+        #         for _ in range(num_items):
+        #             item = df_loc_items.sample(1).to_dict('tight')['data'][0]
+        #             item = tuple(item)
+        #             item_id = insert(conn, SQL_stmt.insert_item, item)
+        #             u_i_id = insert(conn, SQL_stmt.insert_user_item, (user_id, item_id))
 
 
 
--- sql_statements.py
+++ sql_statements.py
@@ -1,46 +1,66 @@
 class SQL_stmt():
     
     create_table_user = """CREATE TABLE IF NOT EXISTS user (
-                                    id integer PRIMARY KEY,
-                                    name text NOT NULL,
-                                    age integer
+                                    id INTEGER PRIMARY KEY,
+                                    name text NOT NULL
                                 );"""
+    
+    create_table_location = """CREATE TABLE IF NOT EXISTS location (
+                                id INTEGER PRIMARY KEY,
+                                name text NOT NULL
+                            );"""
 
 
     create_table_item = """ CREATE TABLE IF NOT EXISTS item (
-                                        id integer PRIMARY KEY,
+                                        id INTEGER PRIMARY KEY,
                                         name text NOT NULL,
                                         abv real NOT NULL,                                      
-                                        kind text NOT NULL,
-                                        amount_l real NOT NULL,
-                                        price real NOL NULL
-                                    ); """
-
-
-    create_table_users_parties = """ CREATE TABLE IF NOT EXISTS users_parties (
-                                        id integer PRIMARY KEY,
-                                        user_id integer NOT NULL,
-                                        party_id integer NOT NULL
+                                        kind text NOT NULL
                                     ); """
     
 
-    create_table_users_items = """ CREATE TABLE IF NOT EXISTS users_items (
-                                        id integer PRIMARY KEY,
-                                        user_id integer NOT NULL,
-                                        item_id integer NOT NULL
+    create_table_party = """ CREATE TABLE IF NOT EXISTS party (
+                                    id INTEGER PRIMARY KEY,
+                                    name text NOT NULL,
+                                    start_dt text, 
+                                    end_dt text
+                                ); """
+
+
+    create_table_party_user_itemloc = """ CREATE TABLE IF NOT EXISTS party_user_itemloc (
+                                        id INTEGER PRIMARY KEY,
+                                        party_id INTEGER NOT NULL,
+                                        user_id INTEGER NOT NULL,
+                                        itemloc_id INTEGER NOT NULL,
+                                        FOREIGN KEY(party_id) REFERENCES party(id),
+                                        FOREIGN KEY(user_id) REFERENCES user(id),
+                                        FOREIGN KEY(itemloc_id) REFERENCES item_location(id)
                                     ); """
     
-    insert_user = """INSERT INTO user (name) VALUES (?);"""
-    insert_party = """INSERT INTO party (name) VALUES (?);"""
-    insert_item = """INSERT INTO item (name, abv, kind, amount_l, price) VALUES (?, ?, ?, ?, ?);"""
 
-    insert_user_party = """INSERT INTO users_parties (user_id, party_id) VALUES (?, ?);"""
-    insert_user_item = """INSERT INTO users_items (user_id, item_id) VALUES (?, ?);"""
+    create_table_item_location = """ CREATE TABLE IF NOT EXISTS item_location (
+                                        id INTEGER PRIMARY KEY,
+                                        item_id INTEGER NOT NULL,
+                                        location_id INTEGER NOT NULL,
+                                        volume real NOT NULL,
+                                        price real NOT NULL,
+                                        order_dt text,
+                                        FOREIGN KEY(item_id) REFERENCES item(id),
+                                        FOREIGN KEY(location_id) REFERENCES location(id)
+                                    ); """
+    
 
-    select_full_data = """select u.id, u.name, i.id, i.name, i.abv, i.kind, i.amount_l, i.price, p.id, p.name, p.location, p.start, p.end 
-                        from user u 
-                        left join users_items ui on u.id = ui.user_id 
-                        left join item i on i.id = ui.item_id 
-                        left join users_parties up on u.id = up.user_id 
-                        left join party p on up.party_id = p.id;"""
+    # insert_user = """INSERT INTO user (name) VALUES (?);"""
+    # insert_party = """INSERT INTO party (name) VALUES (?);"""
+    # insert_item = """INSERT INTO item (name, abv, kind, amount_l, price) VALUES (?, ?, ?, ?, ?);"""
+
+    # insert_user_party = """INSERT INTO users_parties (user_id, party_id) VALUES (?, ?);"""
+    # insert_user_item = """INSERT INTO users_items (user_id, item_id) VALUES (?, ?);"""
+
+    # select_full_data = """select u.id, u.name, i.id, i.name, i.abv, i.kind, i.amount_l, i.price, p.id, p.name, p.location, p.start, p.end 
+    #                     from user u 
+    #                     left join users_items ui on u.id = ui.user_id 
+    #                     left join item i on i.id = ui.item_id 
+    #                     left join users_parties up on u.id = up.user_id 
+    #                     left join party p on up.party_id = p.id;"""
 
